45d7a0c116b474ca80b02c8b92ad56dd41d1a2c6
albi.mori <albi.mori@gmail.com>
2017-06-22 09:58:17 +0200

    setup window

--- ExpenseIt/MainWindow.xaml.cs
+++ ExpenseIt/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace ExpenseIt
     /// <summary>
     /// Logica di interazione per MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : NavigationWindow
     {
         public MainWindow()
         {
