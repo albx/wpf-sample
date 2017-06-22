08c3bb9e0b5d3849653c09ba8c176ff08af6bdd6
albi.mori <albi.mori@gmail.com>
2017-06-22 17:13:23 +0200

    run and kill a process

---
+++ ProcessRunner/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProcessRunner
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var calc = Process.Start("cmd.exe");
+            calc.WaitForExit(5000);
+
+            Thread.Sleep(5000);
+            calc.Kill();
+        }
+    }
+}
